fe503cdfd9883f8e65226c1f16d063ef4d7e4e13
seonyeonghun2 <kjca1117@naver.com>
2025-03-19 13:05:20 +0900

    백엔드 | express 서버 세팅
    1. 빌트인 미들웨어
    - express.json()
    - express.urlencoded()
    
    2. post 라우트 처리
    - req.body 로 전송되는 폼 데이터 출력 및 확인ㅇ

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,9 @@ import cors from "cors";
 import { readUsers } from "./crud-read.js";
 const app = express();
 const port = 3000;
-
+// Built-in Middleware : 요청-응답 사이에 실행되는 특별한 함수
+app.use(express.json()); // axios가 JSON으로 보내온 JSON 데이터를 인식 처리
+app.use(express.urlencoded({ extended: true })); // axios가 JSON으로 보내온 폼 데이터를 인식 처리
 app.use(cors()); // 모든 request에 대한 CORS 에러 처리
 
 app.get("/", (req, res) => {
@@ -19,6 +21,7 @@ app.get("/employees", async (req, res) => {
   }
 });
 app.post("/employees", (req, res) => {
+  console.log(req.body.data);
   res.send("Create Users");
 });
 app.put("/employees/:id", (req, res) => {
