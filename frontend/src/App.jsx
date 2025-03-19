4e2dee1f3ef9fe28879e0a8ff97df4b05a9da395
seonyeonghun2 <kjca1117@naver.com>
2025-03-19 11:56:06 +0900

    주소로 라우팅 처리
    1. Home
    2. List
    3. Add
    
    그밖에 NotFound 처리
    
    4. 기존코드는 routes/ 와 compontents/ 로 분기

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,40 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import {Table} from 'react-bootstrap'
+import { Routes, Route } from 'react-router';
+import Home from './routes/Home';
+import List from './routes/List';
+import Add from './routes/Add';
+import NotFound from './routes/NotFound';
 function App() {
-  const [employees, setEmployees] = useState([]);
-  useEffect(() => {
-    const getEmployees = async () => {
-      const data = await axios.get('http://localhost:3000/employees');
-      // fetch() : json parse 직접 ==> response.json() 호출
-      // axios() : json 자동으로 parse ==> state에 바로 담으면 됨
-      console.log(data);
-      setEmployees(data.data); // setEmployees(data)
-    };
-    getEmployees();
-  }, []);
   return (
-    <div>
-      <h1>사원목록</h1>
-      <table>
-        <tr>
-          <th>사번</th>
-          <th>이름</th>
-          <th>이메일</th>
-          <th>전화번호</th>
-        </tr>
-        {employees.map((emp) => (
-          <tr key={emp._id}>
-            <td>{emp.employee_id}</td>
-            <td>
-              {emp.first_name} {emp.last_name}
-            </td>
-            <td>{emp.email}</td>
-          </tr>
-        ))}
-      </table>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/list" element={<List />} />
+      <Route path="/add" element={<Add />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
 
