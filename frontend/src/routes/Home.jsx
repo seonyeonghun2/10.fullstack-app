e5ec00d10a3487a68568729a19952811aa6d5fb8
seonyeonghun2 <kjca1117@naver.com>
2025-03-19 11:57:19 +0900

    주소별 라우트 페이지
    주소별 라우팅 할 페이지 컴포넌트

---
+++ frontend/src/routes/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Welcome from '../components/Welcome'
+function Home() {
+  return (
+    <Welcome />
+  )
+}
+
+export default Home
---
+++ frontend/src/routes/NotFound.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function NotFound() {
+  return <div>404 NotFound | 페이지를 찾을수 없습니다.</div>;
+}
+
+export default NotFound;
