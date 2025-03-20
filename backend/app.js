bc90ba7589346baa6fb1c3de641b1e0818d04410
seonyeonghun2 <kjca1117@naver.com>
2025-03-20 13:03:32 +0900

    데이터 등록, 삭제 메소드 | 백엔드

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import { readUsers } from "./crud-read.js";
+import { createUser } from "./crud-create.js";
+import { deleteUser } from "./crud-delete.js";
 const app = express();
 const port = 3000;
 // Built-in Middleware : 요청-응답 사이에 실행되는 특별한 함수
@@ -20,15 +22,43 @@ app.get("/employees", async (req, res) => {
     res.status(500).send("DB 연결 오류가 발생했습니다.");
   }
 });
-app.post("/employees", (req, res) => {
-  console.log(req.body.data);
-  res.send("Create Users");
+app.post("/employees", async (req, res) => {
+  console.log(req.body);
+  // res.send("Create Users");
+  try {
+    const result = await createUser(req.body);
+    res.status(201).json({
+      status: "success",
+      data: result,
+    });
+  } catch (e) {
+    console.log("데이터 등록중 에러 : ", e);
+    res.status(500).json({
+      status: "fail",
+      message: "서버 오류로 데이터 등록 실패",
+    });
+  }
 });
 app.put("/employees/:id", (req, res) => {
   res.send("Update Users");
 });
-app.delete("/employees/:id", (req, res) => {
-  res.send("Delete User");
+app.delete("/employees/:id", async (req, res) => {
+  // console.log(req.params);
+  try {
+    const result = await deleteUser({_id: req.params.id});
+    res.status(201).json({
+      status: "success",
+      message: "사원정보가 삭제되었습니다",
+      data: result,
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      status: "fail",
+      message:
+        "삭제요청 실패(데이터가 존재하지 않거나 이미 삭제된 데이터입니다)",
+    });
+  }
 });
 
 app.listen(port, () => {
