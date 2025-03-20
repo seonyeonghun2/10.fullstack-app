09e354a3aa4bb9542f535914b34e469857513015
seonyeonghun2 <kjca1117@naver.com>
2025-03-20 13:02:27 +0900

    url 라우팅 처리

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
-import Welcome from '../components/Welcome'
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col } from 'react-bootstrap';
+import { Link, Outlet } from 'react-router';
 function Home() {
+  const navUl = {
+    listStyle: 'none',
+  };
   return (
-    <Welcome />
-  )
+    <>
+      <header>
+        <Container>
+          <Row className='py-3'>
+            <Col className="col-lg-4">
+              <div className="logo fs-3">한울ERP</div>
+            </Col>
+            <Col className="col-lg-8">
+              <nav>
+                <ul className="d-flex justify-content-around" style={navUl}>
+                  <li>
+                    <Link to="/home">처음으로</Link>
+                  </li>
+                  <li>
+                    <Link to="/list">사원목록</Link>
+                  </li>
+                  <li>
+                    <Link to="/add">사원등록</Link>
+                  </li>
+                  <li>
+                    <Link to="/news">새소식</Link>
+                  </li>
+                </ul>
+              </nav>
+            </Col>
+          </Row>
+        </Container>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+      <footer>
+        <p>&copy; 한울ERP</p>
+      </footer>
+    </>
+  );
 }
 
-export default Home
+export default Home;
--- frontend/src/routes/NotFound.jsx
+++ frontend/src/routes/NotFound.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
-
+// import ErrorScreen from '../assets/error.png';
 function NotFound() {
-  return <div>404 NotFound | 페이지를 찾을수 없습니다.</div>;
+  return (
+    <>
+      <div>404 NotFound | 페이지를 찾을수 없습니다.</div>
+      {/* <ErrorScreen /> */}
+    </>
+  );
 }
 
 export default NotFound;
